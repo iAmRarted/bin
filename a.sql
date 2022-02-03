5e2706f383578afe71946d8086142542832acf0d
iAmRarted <aritz.velasco.pancorbo@gmail.com>
2022-02-03 10:47:36 +0100

    2

--- a.sql
+++ a.sql
@@ -4,13 +4,4 @@ where exists(
     select *
     from tcliente b
     where a.provincia = b.provincia
-    having count(*) > 2
-)
-
-select nombre, provincia
-from tcliente a
-where 1 < (
-    select count(*)
-    from tcliente b
-    where a.provincia = b.provincia
-);
+    having count(*) > 2);

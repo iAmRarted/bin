9e02fc79ff00b1053a86526df353cf5bb15be0a4
iAmRarted <aritz.velasco.pancorbo@gmail.com>
2022-02-03 11:11:12 +0100

    Prueba ordenador remoto

--- a.sql
+++ a.sql
@@ -5,11 +5,3 @@ where exists(
     from tcliente b
     where a.provincia = b.provincia
     having count(*) > 2);
-
-select nombre, provincia
-from tcliente a
-where exists(
-    select *
-    from tcliente b
-    where a.provincia = b.provincia
-    having count(*) > 2);

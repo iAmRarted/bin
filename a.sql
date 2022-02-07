5c06744860b5a74e732e0f3c9791eb96d00f6715
iAmRarted <aritz.velasco.pancorbo@gmail.com>
2022-02-07 13:56:50 +0100

    a

--- a.sql
+++ a.sql
@@ -1,7 +1,24 @@
-select nombre, provincia
-from tcliente a
+select substr(nombre, 1, instr(nombre, ' '))
+from tcliente
 where exists(
     select *
-    from tcliente b
-    where a.provincia = b.provincia
-    having count(*) > 2);
+    from tprovincia
+    where provincia = codigo
+    and 2 <= (
+        select count(*)
+        from tcliente
+        where provincia = codigo
+        and tipo = 'AA'
+    )
+    and 3 = (
+        select count(*)
+        from tcliente
+        where provincia = codigo
+        having sum(compras) > 200000
+    )
+);
+
+select substr(nombre, 1, instr(nombre, ' '))
+from tcliente
+where 
+group by provincia

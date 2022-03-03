bb7faec2fca8700e51a85840ba465a7448fc678b
iAmRarted <aritz.velasco.pancorbo@gmail.com>
2022-03-03 08:25:45 +0100

    cambio login

--- a.sql
+++ a.sql
@@ -1,42 +1,10 @@
--- 1
--- CREATE ROLE c##conectar;
--- grant ALTER SESSION, CREATE CLUSTER, CREATE DATABASE LINK, 
--- CREATE SEQUENCE, CREATE SESSION, CREATE SYNONYM, CREATE TABLE, CREATE VIEW to c##conectar;
-
--- 4
--- create user c##u1 identified by dw1b;
--- create user c##u2 identified by dw1b;
--- create user c##u3 identified by dw1b;
--- create user c##u4 identified by dw1b;
-
--- 5
--- grant connect to c##u1;
--- grant connect to c##u2;
--- grant connect to c##u3;
--- grant connect to c##u4;
-
--- 6
--- grant create table to c##u1;
--- grant create table to c##u2;
--- grant create table to c##u3;
--- grant create table to c##u4;
-
--- 7
--- grant dba to c##u1;
-
--- 8
--- create table T1(c1 number(3));
-
--- 9
--- insert into T1 values(100);
--- insert into T1 values(101);
--- insert into T1 values(102);
--- grant insert, select on T1 to c##u2
--- with grant option;
-
--- 10
--- grant insert, select on c##u1.T1 to c##u3
--- with grant option;
-
--- 11
--- SQL> grant insert, select on c##u1.T1 to c##u4;
+DECLARE 
+    numro number(4):=&numero;
+BEGIN
+    if(numro mod 2) = 0 then
+        dbms_output.put_line('El numero '||numro||' es par');
+    else
+        dbms_output.put_line('El numero '||numro||' es impar');
+    end if;
+END;
+/
--- login.sql
+++ login.sql
@@ -1,3 +1,7 @@
 clear screen
 define_editor="C:\Users\dw1\AppData\Local\Programs\Microsoft VS Code\Code.exe"
 SET LINESIZE 150;
+clear buff;
+set serveroutput on;
+set ver off;
+set echo off;
